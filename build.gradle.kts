3bf31efa8dca9666e859a8a6ceed2e30c399bf1a
mmirk <t-mmirk@hotels.com>
2021-04-20 09:39:56 +0200

    sets development version and name for the project
    - starts from 0.1.0
    - sets name to "semantic-version"
    - cleans gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.polarene"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -14,9 +14,6 @@ repositories {
 
 dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:4.4.3")
-//    testImplementation(kotlin("test-junit5"))
-//    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
 tasks.test {
@@ -27,6 +24,3 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClassName = "MainKt"
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 
-rootProject.name = "SemanticVersion"
+rootProject.name = "semantic-version"
 
