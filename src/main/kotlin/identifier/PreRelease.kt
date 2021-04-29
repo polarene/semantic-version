873fc5142cbd941fa01fdd032bd074afd73162f6
mmirk <t-mmirk@hotels.com>
2021-04-29 17:24:56 +0200

    accepts one or more identifiers, all not empty

--- src/main/kotlin/identifier/PreRelease.kt
+++ src/main/kotlin/identifier/PreRelease.kt
@@ -3,6 +3,7 @@ package io.github.semver.identifier
 class PreRelease(val identifier: String, vararg val identifiers: String) {
     init {
         require(identifier.isNotBlank())
+        require(identifiers.all { it.isNotBlank() })
     }
 
     override fun toString(): String = identifier
--- src/test/kotlin/identifier/PreReleaseTest.kt
+++ src/test/kotlin/identifier/PreReleaseTest.kt
@@ -1,5 +1,6 @@
 package io.github.semver.identifier
 
+import io.kotest.assertions.assertSoftly
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
 
@@ -7,9 +8,22 @@ class PreReleaseTest : StringSpec({
     "should create a pre-release with at least one identifier" {
         PreRelease("alpha")
         PreRelease("rc", "2")
+        PreRelease("0", "2", "5")
     }
 
     "should not allow empty identifiers" {
-        shouldThrow<IllegalArgumentException> { PreRelease("") }
+        assertSoftly {
+            shouldThrow<IllegalArgumentException> { PreRelease("") }
+            shouldThrow<IllegalArgumentException> { PreRelease("rc", "") }
+            shouldThrow<IllegalArgumentException> { PreRelease("alpha", "1", "") }
+        }
+    }
+
+    "should not allow blank identifiers" {
+        assertSoftly {
+            shouldThrow<IllegalArgumentException> { PreRelease(" ") }
+            shouldThrow<IllegalArgumentException> { PreRelease("rc", " ") }
+            shouldThrow<IllegalArgumentException> { PreRelease("alpha", "1", " ") }
+        }
     }
 })
