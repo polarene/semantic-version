b64b8fd36cb63b61c2bfdb0462f44f3db0d2888c
mmirk <mmirk@expediagroup.com>
2024-01-25 12:53:55 +0100

    corrects an error message

--- src/main/kotlin/identifier/PreRelease.kt
+++ src/main/kotlin/identifier/PreRelease.kt
@@ -7,7 +7,7 @@ class PreRelease(vararg val identifiers: String) : Comparable<PreRelease> {
     init {
         require(identifiers.isNotEmpty()) { "pre-release identifiers must not be empty" }
         require(identifiers.all(charset::matches)) { "pre-release identifiers must match $charset: $identifiers" }
-        require(numericWithoutLeadingZeroes()) { "pre-release numric identifiers must not have leading zeroes: $identifiers" }
+        require(numericWithoutLeadingZeroes()) { "pre-release numeric identifiers must not have leading zeroes: $identifiers" }
     }
 
     private fun numericWithoutLeadingZeroes() =
